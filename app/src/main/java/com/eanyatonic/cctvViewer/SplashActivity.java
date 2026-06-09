3e98e018f300db45beea100a3e5e03eacc92a18f
YU-HONG-BO <2018258010@qq.com>
2026-06-09 16:32:23 +0800

    Update SplashActivity.java

--- app/src/main/java/com/eanyatonic/cctvViewer/SplashActivity.java
+++ app/src/main/java/com/eanyatonic/cctvViewer/SplashActivity.java
@@ -11,11 +11,10 @@ public class SplashActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
 
-        // 3秒后自动跳转到主界面
         new Handler(getMainLooper()).postDelayed(() -> {
             Intent intent = new Intent(this, MainActivity.class);
             startActivity(intent);
             finish();
-        }, 3000); // 3000毫秒=3秒
+        }, 3000);
     }
 }
